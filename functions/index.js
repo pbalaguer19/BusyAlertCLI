dd38b687f39c45a2514d210169ef0c7c26473fdf
Pau <pbalaguer19@gmail.com>
2017-04-20 12:52:50 +0200

    CheckUserStatus seems working

--- functions/index.js
+++ functions/index.js
@@ -2,28 +2,36 @@ var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
-
+var db = admin.database();
 
 exports.checkUserStatus = functions.database.ref('/users/{userID}/status')
     .onWrite(event => {
       const snapshot = event.data;
+      if(snapshot.val() != "Busy") return;
+
       const userID = event.params.userID;
-      if (snapshot.previous.val()) {
-        return;
-      }
 
       const getUserProfilePromise = admin.auth().getUser(userID);
+      const getFriendsTokenPromise = db.ref('/users-favourites').child(userID).once('value');
 
-      return Promise.all([getUserProfilePromise]).then(results => {
+      return Promise.all([getUserProfilePromise, getFriendsTokenPromise]).then(results => {
         const user = results[0];
-        const tokens = loadFavourites(userID);
+        const phonesSnap = results[1];
 
+        if (!phonesSnap.exists()) return;
+
+        const tokens = [];
+        phonesSnap.forEach(function(data) {
+            tokens.push(Object.keys(phonesSnap.child(data.key).val())[0]);
+        });
+
+        console.log("Sending Notification to " + tokens);
         if(tokens.length > 0){
           // Notification details.
           const payload = {
             notification: {
               title: 'Busy Notification!',
-              body: `${user.username} is busy now.`,
+              body: `${user.email} is busy now.`,
             }
           };
 
@@ -44,33 +52,3 @@ exports.checkUserStatus = functions.database.ref('/users/{userID}/status')
         }
       });
 });
-
-function loadFavourites(uid) {
-    return functions.database.child('users-favourites').child(uid).once('value').then(function(snapshot) {
-        var reads = [];
-        snapshot.forEach(function(childSnapshot) {
-            var telf = childSnapshot.key();
-
-            var id = functions.database.child('users-phone').child(telf).once('value').then(function(snap) {
-                // The Promise was fulfilled.
-            }, function(error) {
-                // The Promise was rejected.
-                console.error(error);
-            });
-
-            var promise = functions.database.child('users').child(id).child("token").once('value').then(function(snap) {
-                // The Promise was fulfilled.
-            }, function(error) {
-                // The Promise was rejected.
-                console.error(error);
-            });
-            reads.push(promise);
-        });
-        return Promise.all(reads);
-    }, function(error) {
-        // The Promise was rejected.
-        console.error(error);
-    }).then(function(values) {
-        console.log(values); // [snap, snap, snap]
-    });
-}
